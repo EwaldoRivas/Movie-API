569ac49f501a040c1f56e163d8937efd72567e54
Ewaldo <EwaldoRivas14@gmail.com>
2024-09-16 01:58:01 -0400

    Migrated to PostgreSQL and configured psycopg2-binary

--- config/database.py
+++ config/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # URL de conexión de PostgreSQL proporcionada por Railway
-database_url = "postgresql://postgres:LbBPmcnazifjCXmtxCHJWHAxSifaImnn@junction.proxy.rlwy.net:53034/railway"
+database_url = "postgresql://postgres:LbBPmcnazifjCXmtxCHJWHAxSifaImnn@postgres.railway.internal:5432/railway"
 
 engine = create_engine(database_url, echo=True)
 
